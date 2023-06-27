66c689f4aae8133b28948729a86f72e244a193a9
kozukioden <47193697+bosinupebi@users.noreply.github.com>
2023-06-27 07:12:16 -0400

    Update buidler.config.js

--- buidler.config.js
+++ buidler.config.js
@@ -4,7 +4,7 @@ require('./scripts/moloch-tasks')
 require('./scripts/pool-tasks')
 
 const INFURA_API_KEY = ''
-const MAINNET_PRIVATE_KEY = '80c3f2356eef717adc0feb841c8b59c7a0dd83c9a38464b0aa0de45e9d35d8a2'
+const MAINNET_PRIVATE_KEY '
 const ROPSTEN_PRIVATE_KEY = ''
 
 module.exports = {
